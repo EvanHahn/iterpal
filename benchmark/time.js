955d6ae429890eeeb20f1c2d180580f69d8a779f
Evan Hahn <me@evanhahn.com>
2019-05-05 18:47:17 -0500

    Add some simple benchmarks for `map`

---
+++ benchmark/time.js
@@ -0,0 +1,9 @@
+module.exports = function time (name, fn) {
+  process.stdout.write(name)
+  process.stdout.write('...')
+  const start = process.hrtime.bigint()
+  fn()
+  const finish = process.hrtime.bigint()
+  const ms = (finish - start) / BigInt(1000000)
+  process.stdout.write(`completed in ${ms}ms\n`)
+}
