430d67aadcd5ee708b62f51ba17d3044f6c457e8
Evan Hahn <me@evanhahn.com>
2019-05-05 19:58:31 -0500

    at

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## Unreleased
 ### Added
+- `at`
 - `concat`
 - 'filter'
 - `first`
---
+++ at.js
@@ -0,0 +1,14 @@
+module.exports = function at (iterable, desiredIndex) {
+  const iterator = iterable[Symbol.iterator]()
+
+  for (let i = 0; i <= desiredIndex; i++) {
+    const iteration = iterator.next()
+    if (i === desiredIndex) {
+      return iteration.value
+    } else if (iteration.done) {
+      break
+    }
+  }
+
+  return undefined
+}
