213eb3b33a9484e7ae6141466934412e065a1935
Evan Hahn <me@evanhahn.com>
2019-05-09 23:36:18 -0500

    Minor: flesh out comment in `size`

--- size.js
+++ size.js
@@ -1,5 +1,6 @@
 module.exports = function size (iterable) {
-  // These are for performance.
+  // These are for performance, saving us from having to go through
+  // the whole iterable for some common cases.
   if (Array.isArray(iterable) || (typeof iterable === 'string')) {
     return iterable.length
   } else if ((iterable instanceof Set) || (iterable instanceof Map)) {
