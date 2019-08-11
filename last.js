b7e58d5d717bdd555a05483573d17f62d63c7e3f
Evan Hahn <me@evanhahn.com>
2019-08-11 12:05:43 -0500

    last: add a performance improvement for arrays and strings

--- last.js
+++ last.js
@@ -1,4 +1,10 @@
 module.exports = function (iterable) {
+  // These are for performance, saving us from having to go through
+  // the whole iterable for some common cases.
+  if (Array.isArray(iterable) || (typeof iterable === 'string')) {
+    return iterable[iterable.length - 1]
+  }
+
   let result
   for (const element of iterable) {
     result = element
