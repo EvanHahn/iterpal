181dcafb5a8a49f09d860c2576b4e4c27df445fa
Evan Hahn <me@evanhahn.com>
2020-12-11 09:43:13 -0600

    Update devDependencies to latest versions

--- benchmark/time_to_start_iterpal.benchmark.js
+++ benchmark/time_to_start_iterpal.benchmark.js
@@ -5,6 +5,8 @@ import map from '../map.js'
 const bigArray = Array(5000000).fill(null).map(Math.random)
 
 time('starting to read the first element from an iterpal-mapped array', () => {
+  // This unreachable loop is deliberate.
+  // eslint-disable-next-line no-unreachable-loop
   for (const value of map(bigArray, String)) {
     return value
   }
--- benchmark/time_to_start_native.benchmark.js
+++ benchmark/time_to_start_native.benchmark.js
@@ -3,6 +3,8 @@ import time from './time.js'
 const bigArray = Array(5000000).fill(null).map(Math.random)
 
 time('starting to read the first element from a natively-mapped array', () => {
+  // This unreachable loop is deliberate.
+  // eslint-disable-next-line no-unreachable-loop
   for (const value of bigArray.map(String)) {
     return value
   }
