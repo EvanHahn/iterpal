fd462bad8b09dcea386bc8875b75b95164c3b2cc
Evan Hahn <me@evanhahn.com>
2024-01-23 13:36:19 -0600

    Convert every to TypeScript

--- every.js
+++
@@ -1,9 +0,0 @@
-export default function every(iterable, predicate) {
-  for (const value of iterable) {
-    if (!predicate(value)) {
-      return false;
-    }
-  }
-
-  return true;
-}
--- test/every.test.ts
+++ test/every.test.ts
@@ -1,6 +1,8 @@
 import { assert, assertFalse } from "assert";
 
-import every from "../every.js";
+import every from "../every.ts";
+
+const isThree = (n: number) => n === 3;
 
 Deno.test("returns true for empty iterables", () => {
   const alwaysFalse = () => false;
@@ -9,16 +11,12 @@ Deno.test("returns true for empty iterables", () => {
 });
 
 Deno.test("returns true if all of the elements return true", () => {
-  const isThree = (n) => n === 3;
-
   assert(every([3], isThree));
   assert(every(new Set([3, 3]), isThree));
   assert(every([3, 3, 3], isThree));
 });
 
 Deno.test("returns false if every of the elements return false", () => {
-  const isThree = (n) => n === 3;
-
   assertFalse(every([1, 2, 3, 4], isThree));
   assertFalse(every(new Set([1, 2, 3, 4]), isThree));
 });
