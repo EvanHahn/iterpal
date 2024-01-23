ef3c037228a3af0859abce38bb0d9158e6eb3402
Evan Hahn <me@evanhahn.com>
2024-01-23 16:44:45 -0600

    Document some

--- some.ts
+++ some.ts
@@ -1,3 +1,23 @@
+/**
+ * Returns true if at least one element matches the predicate, and false otherwise. Similar to [Array.prototype.some](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some). Returns false for empty iterables.
+ *
+ * @example
+ * ```typescript
+ * function isEven(n) {
+ *   return n % 2 === 0;
+ * }
+ *
+ * const mySet = new Set([1, 2, 3]);
+ * some(mySet, isEven);
+ * // => true
+ *
+ * some([1, 3, 5], isEven);
+ * // => false
+ *
+ * some([], () => true);
+ * // => false
+ * ```
+ */
 export default function some<T>(
   iterable: Iterable<T>,
   predicate: (value: T) => boolean,
