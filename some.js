fbfcf7ba37184fab31209e8adb0bbd8faeb61617
Evan Hahn <me@evanhahn.com>
2024-01-23 15:11:32 -0600

    Convert some to TypeScript

--- some.js
+++
@@ -1,9 +0,0 @@
-export default function some(iterable, predicate) {
-  for (const value of iterable) {
-    if (predicate(value)) {
-      return true;
-    }
-  }
-
-  return false;
-}
---
+++ some.ts
@@ -0,0 +1,9 @@
+export default <T>(
+  iterable: Iterable<T>,
+  predicate: (value: T) => boolean,
+): boolean => {
+  for (const value of iterable) {
+    if (predicate(value)) return true;
+  }
+  return false;
+};
