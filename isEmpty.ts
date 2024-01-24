0764bc06b73f285f9438fe5d5365e857abf17b09
Evan Hahn <me@evanhahn.com>
2024-01-23 23:41:20 -0600

    Document isEmpty

--- isEmpty.ts
+++ isEmpty.ts
@@ -1,3 +1,15 @@
+/**
+ * Returns `true` if `iterable` has no elements, and `false` otherwise.
+ *
+ * @example
+ * ```typescript
+ * isEmpty(new Set());
+ * // => true
+ *
+ * isEmpty([1, 2, 3]);
+ * // => false
+ * ```
+ */
 export default function isEmpty(iterable: Iterable<unknown>): boolean {
   return Boolean(iterable[Symbol.iterator]().next().done);
 }
