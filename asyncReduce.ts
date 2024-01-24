8abae12ef20320d7ebccf26819e1996dbc3f678b
Evan Hahn <me@evanhahn.com>
2024-01-23 23:44:26 -0600

    Add explicit return type to asyncReduce

--- asyncReduce.ts
+++ asyncReduce.ts
@@ -15,7 +15,7 @@ export default async function asyncReduce<T, U>(
   asyncIterable: AsyncIterable<T>,
   fn: (previousValue: U, currentValue: T) => U | Promise<U>,
   accumulator: U,
-) {
+): Promise<U> {
   let result = accumulator;
   for await (const value of asyncIterable) {
     result = await fn(result, value);
