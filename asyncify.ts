26f28b90152149f4a15ac795a384915671399a96
Evan Hahn <me@evanhahn.com>
2024-01-24 00:00:36 -0600

    Update asyncify to support copies of async iterables

--- asyncify.ts
+++ asyncify.ts
@@ -1,5 +1,35 @@
-export default function asyncify<T>(iterable: Iterable<T>): AsyncIterable<T> {
-  return new AsyncifyIterable(iterable);
+/**
+ * Make an iterable async.
+ *
+ * If the iterable is synchronous (e.g., defines `Symbol.iterator`), converts it to an async iterable. If the iterable is asynchronous (e.g., defines `Symbol.asyncIterator`), a copy is returned.
+ *
+ * In the unlikely event that both are defined, a copy of the async "part" is returned and the sync part is ignored.
+ *
+ * @example
+ * ```typescript
+ * asyncify([1, 2, 3, 4]);
+ * // => Asynchronous iterable yielding 1, 2, 3, 4
+ * ```
+ */
+export default function asyncify<T>(
+  iterable: Iterable<T> | AsyncIterable<T>,
+): AsyncIterable<T> {
+  return (Symbol.asyncIterator in iterable)
+    ? new CopyAsyncIterable(iterable)
+    : new AsyncifyIterable(iterable);
+}
+
+class CopyAsyncIterable<T> implements AsyncIterable<T> {
+  #iterable: AsyncIterable<T>;
+
+  constructor(iterable: AsyncIterable<T>) {
+    this.#iterable = iterable;
+  }
+
+  [Symbol.asyncIterator]() {
+    const iterator = this.#iterable[Symbol.asyncIterator]();
+    return { next: () => iterator.next() };
+  }
 }
 
 class AsyncifyIterable<T> implements AsyncIterable<T> {
--- test/asyncify.test.ts
+++ test/asyncify.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "assert";
+import { assertEquals, assertNotStrictEquals } from "assert";
 import asyncIterableToArray from "../asyncIterableToArray.ts";
 
 import asyncify from "../asyncify.ts";
@@ -31,4 +31,31 @@ Deno.test("converts a sync infinite iterable to an async one", async () => {
   assertEquals(await iterator.next(), { done: false, value: 2 });
   assertEquals(await iterator.next(), { done: false, value: 3 });
   assertEquals(await iterator.next(), { done: false, value: 4 });
+});
+
+Deno.test("copies async iterables", async () => {
+  const input = asyncify([1, 2, 3]);
+
+  const result = asyncify(input);
+
+  assertNotStrictEquals(result, input);
+
+  assertEquals(await asyncIterableToArray(result), [1, 2, 3]);
+});
+
+Deno.test('if object is both kinds of iterable, "prefers" async iterables when making copy', async () => {
+  const both = {
+    [Symbol.iterator]() {
+      throw new Error("This should never be called");
+    },
+    *[Symbol.asyncIterator]() {
+      yield 1;
+      yield 2;
+      yield 3;
+    },
+  };
+
+  const result = asyncify(both);
+
+  assertEquals(await asyncIterableToArray(result), [1, 2, 3]);
 });
