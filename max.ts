75c4d73d047bb242c6bf3a98be93232a5b37a0f7
Evan Hahn <me@evanhahn.com>
2024-01-23 23:40:17 -0600

    Document min and max

--- max.ts
+++ max.ts
@@ -1,3 +1,15 @@
+/**
+ * Returns the largest number in the iterable `numbers`. Returns `undefined` if `numbers` is an empty iterable. `numbers` can contain numbers or bigints.
+ *
+ * @example
+ * ```typescript
+ * max(new Set([9, 3, 1]));
+ * // => 9
+ *
+ * max([Infinity, 1, 2]);
+ * // => Infinity
+ * ```
+ */
 export default function max<T extends number | bigint>(
   numbers: Iterable<T>,
 ): undefined | T {
--- min.ts
+++ min.ts
@@ -1,4 +1,16 @@
-export default function <T extends number | bigint>(
+/**
+ * Returns the smallest number in the iterable `numbers`. Returns `undefined` if `numbers` is an empty iterable. `numbers` can contain numbers or bigints.
+ *
+ * @example
+ * ```typescript
+ * min(new Set([9, 3, 1]));
+ * // => 1
+ *
+ * min([-Infinity, 1, 2]);
+ * // => -Infinity
+ * ```
+ */
+export default function min<T extends number | bigint>(
   numbers: Iterable<T>,
 ): undefined | T {
   let result: undefined | T;
