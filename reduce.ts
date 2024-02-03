da7574ba110af61fd528bf7de5e0c33dcac283b5
Evan Hahn <me@evanhahn.com>
2024-02-03 15:39:19 -0600

    Document reduce

--- reduce.ts
+++ reduce.ts
@@ -1,3 +1,14 @@
+/**
+ * Reduces `iterable` to a single value. On each iteration, calls `fn` with the result so far (starting at `accumulator`) and the current value.
+ *
+ * Similar to `Array.prototype.reduce`.
+ *
+ * @example
+ * ```javascript
+ * reduce(numbers, (total, n) => total + n, 0);
+ * // => 1234
+ * ```
+ */
 export default function reduce<T, U>(
   iterable: Iterable<T>,
   fn: (previousValue: U, currentValue: T) => U,
