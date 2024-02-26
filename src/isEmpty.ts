863170dd645058660acf4eb3bd21a631739616a0
Evan Hahn <me@evanhahn.com>
2024-02-25 20:36:16 -0600

    isEmpty now handles sync and async

--- src/isEmpty.ts
+++ src/isEmpty.ts
@@ -1,5 +1,17 @@
+import isSync from "./_isSync.ts";
+
+/** @ignored */
+export default function isEmpty(iterable: Iterable<unknown>): boolean;
+
+/** @ignored */
+export default function isEmpty(
+  iterable: AsyncIterable<unknown>,
+): Promise<boolean>;
+
 /**
  * Returns `true` if `iterable` has no elements, and `false` otherwise.
+ *
+ * Works with sync and async iterables. If passed an async iterable, returns a Promise for the result.
  *
  * @example
  * ```typescript
@@ -10,6 +22,12 @@
  * // => false
  * ```
  */
-export default function isEmpty(iterable: Iterable<unknown>): boolean {
-  return Boolean(iterable[Symbol.iterator]().next().done);
+export default function isEmpty(
+  iterable: Iterable<unknown> | AsyncIterable<unknown>,
+): boolean | Promise<boolean> {
+  return isSync(iterable)
+    ? Boolean(iterable[Symbol.iterator]().next().done)
+    : iterable[Symbol.asyncIterator]()
+      .next()
+      .then(({ done }) => Boolean(done));
 }
--- test/isEmpty.test.ts
+++ test/isEmpty.test.ts
@@ -1,8 +1,8 @@
 import { assert, assertFalse } from "assert";
 
-import { isEmpty } from "../mod.ts";
+import { asyncify, emptyAsyncIterable, isEmpty } from "../mod.ts";
 
-Deno.test("returns true for empty iterables", () => {
+Deno.test("returns true for empty iterables", async () => {
   const customEmpty = {
     *[Symbol.iterator]() {},
   };
@@ -11,19 +11,33 @@ Deno.test("returns true for empty iterables", () => {
   assert(isEmpty(new Set()));
   assert(isEmpty(new Map()));
   assert(isEmpty(customEmpty));
+
+  assert(await isEmpty(emptyAsyncIterable));
 });
 
-Deno.test("returns false for non-empty iterables", () => {
-  const everyNumber = {
+Deno.test("returns false for non-empty iterables", async () => {
+  const everyNumberSync = {
     *[Symbol.iterator]() {
       for (let i = 0; true; i++) {
         yield i;
       }
     },
   };
-
   assertFalse(isEmpty([1]));
   assertFalse(isEmpty(new Set([1])));
   assertFalse(isEmpty(new Map([["hi", 5]])));
-  assertFalse(isEmpty(everyNumber));
+  assertFalse(isEmpty(everyNumberSync));
+
+  assertFalse(await isEmpty(asyncify([1])));
+  assertFalse(await isEmpty(asyncify(everyNumberSync)));
+});
+
+Deno.test("prefers sync iterables", () => {
+  const hybrid: Iterable<unknown> & AsyncIterable<unknown> = {
+    *[Symbol.iterator]() {},
+    async *[Symbol.asyncIterator]() {
+      yield* [1, 2, 3];
+    },
+  };
+  assert(isEmpty(hybrid));
 });
