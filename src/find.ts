dbf2a1d0a6d58627f7f2a22af1ad986052d31f6b
Evan Hahn <me@evanhahn.com>
2024-02-29 12:34:07 -0600

    find now handles sync and async

--- src/find.ts
+++ src/find.ts
@@ -1,5 +1,23 @@
+import isSync from "./_isSync.ts";
+
+export default find;
+
+/** @ignored */
+function find<T>(
+  iterable: Iterable<T>,
+  predicate: (value: T) => boolean,
+): undefined | T;
+
+/** @ignored */
+function find<T>(
+  iterable: AsyncIterable<T>,
+  predicate: (value: T) => boolean,
+): Promise<undefined | T>;
+
 /**
  * Iterates over `iterable`, returning the first element `predicate(value)` returns truthy for. Returns `undefined` if no value is found.
+ *
+ * Works with sync and async iterables. If passed an async iterable, returns a Promise for the result.
  *
  * @example
  * ```typescript
@@ -15,11 +33,30 @@
  * // => undefined
  * ```
  */
-export default function find<T>(
+function find<T>(
+  iterable: Iterable<T> | AsyncIterable<T>,
+  predicate: (value: T) => boolean,
+): undefined | T | Promise<undefined | T> {
+  return isSync(iterable)
+    ? findSync(iterable, predicate)
+    : findAsync(iterable, predicate);
+}
+
+function findSync<T>(
   iterable: Iterable<T>,
   predicate: (value: T) => boolean,
-): T | undefined {
+): undefined | T {
   for (const value of iterable) {
+    if (predicate(value)) return value;
+  }
+  return;
+}
+
+async function findAsync<T>(
+  iterable: AsyncIterable<T>,
+  predicate: (value: T) => boolean,
+): Promise<undefined | T> {
+  for await (const value of iterable) {
     if (predicate(value)) return value;
   }
   return;
--- test/find.test.ts
+++ test/find.test.ts
@@ -1,22 +1,39 @@
 import { assertEquals } from "assert";
 
-import { find } from "../mod.ts";
+import { asyncify, emptyAsyncIterable, find } from "../mod.ts";
 
 const isTen = (n: number) => n === 10;
 
-Deno.test("finds the first matching element and returns it", () => {
-  const everyNumber = {
+Deno.test("finds the first matching element and returns it", async () => {
+  const everyNumberSync = {
     *[Symbol.iterator]() {
       for (let i = 5; true; i++) {
         yield i;
       }
     },
   };
+  assertEquals(find(everyNumberSync, isTen), 10);
 
-  assertEquals(find(everyNumber, isTen), 10);
+  const everyNumberAsync = asyncify(everyNumberSync);
+  assertEquals(await find(everyNumberAsync, isTen), 10);
 });
 
-Deno.test("returns undefined if the value is not found", () => {
+Deno.test("returns undefined if the value is not found", async () => {
   assertEquals(find([], isTen), undefined);
   assertEquals(find([1, 2, 3], isTen), undefined);
+
+  assertEquals(await find(emptyAsyncIterable, isTen), undefined);
+  assertEquals(await find(asyncify([1, 2, 3]), isTen), undefined);
+});
+
+Deno.test("prefers sync iterables", () => {
+  const hybrid: Iterable<number> & AsyncIterable<number> = {
+    *[Symbol.iterator]() {
+      yield* [12, 11, 10];
+    },
+    async *[Symbol.asyncIterator]() {
+      yield* [1, 2, 3];
+    },
+  };
+  assertEquals(find(hybrid, isTen), 10);
 });
